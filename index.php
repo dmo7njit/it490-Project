c4bec14ae55ef5ad6a95d7c4b46d8c8ba3813db2
David O'Grady <dmo7@njit.edu>
2016-02-25 20:12:42 -0500

    adding comments

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <html>
-	<head>
+	<head> 
 		<title>IT490 Group A - Air Traffic Monitoring System</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="style.css"> //style sheet link
 	</head>
 <body>
 
 <?php
 	error_reporting(-1);
-	ini_set('display_errors','On');
+	ini_set('display_errors','On'); //allows for error reporting
 	
+	//makes an object connection using PDO to my mysql db hosted at njit
 	$db = new PDO('mysql:host=sql.njit.edu;dbname=dmo7;charset=utf8','dmo7','compute23',array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	
+	//includes the "work horse" pages.. 
 	include 'pages.php';
 	
+	//sets if there is a GET REQUEST to retrieve the corrisponding page
+	//else it shows the "default" page which is the menu
 	if(isset($_GET['page'])){
 		$qry = new $_GET['page'];
 		
