8381ff497cbb061b952d9bb099a41aa36f7fbcc7
David O'Grady <dmo7@njit.edu>
2016-02-25 20:16:36 -0500

    commenting

--- pages.php
+++ pages.php
@@ -1,9 +1,14 @@
+//takes in the the GET request and constructs the "class" based on the request.
+
 <?php
 	class outbound{
 		function __construct(){
+			//creates the DB object
 			global $db;
+			//creates the pdo query to be sent
 			$qry = $db->query('SELECT * FROM aircraft;');
 			echo "<h2> Outbound Flights </h2>";
+			//creates table that will return the results based on length of items in query
 			echo "<table>";
 			echo "<tr><td><b> tail number </b></td><td><b> craft type </b></td><td><b> fuel </b></td></tr><td><b> pilot </b></td>";
 			while($value = $qry->fetch(PDO::FETCH_BOTH)){
